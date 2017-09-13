5f757ac6b7b9f7ded8136b3701181b5abf5270a9
carli stoliar <carli@carlis-MacBook-Pro.local>
2017-09-13 13:38:26 -0700

    fixed breakpoints

--- src/app/sandbox/breakpoints.component.ts
+++ src/app/sandbox/breakpoints.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
+import {log} from "util";
+
+
 
 @Component({
   selector: 'app-breakpoints',
   templateUrl: './breakpoints.component.html',
   styleUrls: ['./breakpoints.component.css']
 })
-export class BreakpointsComponent implements OnInit {
+export class BreakpointsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+
+  }
+
+  ngOnDestroy(){
+    document.body.style.background = "white";
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event){
+    if (event.target.innerWidth < 576) {
+      document.body.style.background = "white";
+    }
+    if (event.target.innerWidth > 576 && event.target.innerWidth < 768) {
+      document.body.style.background = "red";
+    }
+    if (event.target.innerWidth > 768 && event.target.innerWidth < 992) {
+      document.body.style.background = "blue";
+    }
+    if (event.target.innerWidth > 992 && event.target.innerWidth < 1200) {
+      document.body.style.background = "green";
+    }
+    if (event.target.innerWidth > 1200) {
+      document.body.style.background = "yellow";
+    }
+
   }
 
 }
